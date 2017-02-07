f0fdca56bc50543c40a1ea70a27d73dceafc2ec4
kgmfigueroa <kathleengay.figueroa@gmail.com>
2017-02-07 17:25:38 +0800

    Play audio using MediaPlayer with media controls

--- app/src/main/java/com/aclass/edx/helloworld/VideoActivity.java
+++ app/src/main/java/com/aclass/edx/helloworld/VideoActivity.java
@@ -2,13 +2,18 @@ package com.aclass.edx.helloworld;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Handler;
+import android.widget.MediaController;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.VideoView;
 
-public class VideoActivity extends AppCompatActivity {
+public class VideoActivity extends AppCompatActivity implements MediaController.MediaPlayerControl {
 
     private MediaPlayer mediaPlayer;
+    private MediaController mediaController;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +25,36 @@ public class VideoActivity extends AppCompatActivity {
 
         TextView filename = (TextView) findViewById(R.id.filename);
         filename.setText(selectedFilename);
-    }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
+        VideoView video = (VideoView) findViewById(R.id.videoView);
+
         mediaPlayer = MediaPlayer.create(this, R.raw.video1);
-        mediaPlayer.start();
+        mediaController = new MediaController(VideoActivity.this);
+        mediaController.setAnchorView(video);
+        mediaController.setMediaPlayer(this);
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mediaPlayer.start();
+
+
+                handler.post(new Runnable(){
+                    @Override
+                    public void run() {
+                        mediaController.setEnabled(true);
+                        mediaController.show(0);
+                    }
+                });
+            }
+        });
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        mediaPlayer.start();
+    protected void onStop() {
+        super.onStop();
+        mediaPlayer.stop();
+        mediaPlayer.release();
     }
 
     @Override
@@ -50,9 +72,58 @@ public class VideoActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        mediaPlayer.stop();
-        mediaPlayer.release();
+    public void start() {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void pause() {
+        mediaPlayer.pause();
+    }
+
+    @Override
+    public int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        mediaPlayer.seekTo(pos);
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return mediaPlayer.getAudioSessionId();
     }
 }
