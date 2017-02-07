6f14f7397f8e72bf8c8e7e6ebc33819117dcd5f1
kgmfigueroa <kathleengay.figueroa@gmail.com>
2017-02-07 15:35:31 +0800

    Play one audio file for any clicked item

--- app/src/main/java/com/aclass/edx/helloworld/VideoActivity.java
+++ app/src/main/java/com/aclass/edx/helloworld/VideoActivity.java
@@ -1,11 +1,15 @@
 package com.aclass.edx.helloworld;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class VideoActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private int currentPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,42 @@ public class VideoActivity extends AppCompatActivity {
 
         TextView filename = (TextView) findViewById(R.id.filename);
         filename.setText(selectedFilename);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.closing);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mediaPlayer.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        currentPosition = mediaPlayer.getCurrentPosition();
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.seekTo(currentPosition);
+        mediaPlayer.start();
     }
 }
