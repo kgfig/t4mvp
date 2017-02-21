62080db472e9a82761b5b9f7ee286477a62b1588
kgmfigueroa <kathleengay.figueroa@gmail.com>
2017-02-21 12:21:50 +0800

    Replace MediaController with regular Button view

--- app/src/main/java/com/aclass/edx/helloworld/AudioRecorderActivity.java
+++ app/src/main/java/com/aclass/edx/helloworld/AudioRecorderActivity.java
@@ -1,14 +1,17 @@
 package com.aclass.edx.helloworld;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,7 +26,7 @@ import java.util.Random;
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
-public class AudioRecorderActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnPreparedListener, MediaController.MediaPlayerControl {
+public class AudioRecorderActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
 
     private static final String TAG = AudioRecorderActivity.class.getSimpleName();
     private static final int REQUEST_PERMISSION_CODE = 1;
@@ -31,25 +34,24 @@ public class AudioRecorderActivity extends AppCompatActivity implements View.OnC
     private static final int RECORD_PERMISSION = 1;
 
     // Status and activity vars
-    private String[] activityPermissions = new String[]{ WRITE_EXTERNAL_STORAGE, RECORD_AUDIO};
+    private String[] activityPermissions = new String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO};
     private boolean permissionToRecordGranted = false, permissionToStoreGranted = false;
-    private boolean isRecording = false;
+    private boolean isRecording = false, isPlaying = false;
     private String audioFilename, action;
 
     // Views
-    private Button recordButton;
+    private Button recordButton, playButton;
     private TextView recordedFilenameView;
 
     // Media stuff
     private MediaRecorder mediaRecorder;
     private MediaPlayer mediaPlayer;
-    private MediaController mediaController;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        switch(requestCode) {
+        switch (requestCode) {
             case REQUEST_PERMISSION_CODE:
                 if (grantResults.length >= 2) {
                     permissionToStoreGranted = grantResults[STORE_PERMISSION] == PackageManager.PERMISSION_GRANTED;
@@ -58,6 +60,15 @@ public class AudioRecorderActivity extends AppCompatActivity implements View.OnC
                 break;
         }
 
+        try {
+            if (permissionToStoreGranted && permissionToRecordGranted) {
+                startRecording();
+            } else {
+                finish();
+            }
+        } catch (IOException e) {
+            recordedFilenameView.setText("Error recording " + e.getMessage());
+        }
     }
 
     @Override
@@ -68,12 +79,14 @@ public class AudioRecorderActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_audio_recorder);
 
         recordButton = (Button) findViewById(R.id.record_button);
+        playButton = (Button) findViewById(R.id.play_button);
         recordedFilenameView = (TextView) findViewById(R.id.recorded_file);
-        mediaController = new MediaController(this);
 
         recordButton.setOnClickListener(this);
-        mediaController.setAnchorView(recordedFilenameView);
-        mediaController.setMediaPlayer(this);
+        playButton.setOnClickListener(this);
+
+        playButton.setEnabled(false);
+
     }
 
     @Override
@@ -88,26 +101,34 @@ public class AudioRecorderActivity extends AppCompatActivity implements View.OnC
             stopPlaying();
             mediaPlayer.release();
             mediaPlayer = null;
+        }
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.record_button:
+                onClickRecord();
+                break;
+            case R.id.play_button:
+                onClickPlay();
+                break;
+            default:
+                recordedFilenameView.setText("Do nothing");
+                break;
         }
     }
 
     @Override
     public void onPrepared(MediaPlayer mp) {
-        mediaPlayer.start();
-        mediaController.show();
+        playButton.setEnabled(true);
     }
 
     @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.record_button:
-                onClickRecord();
-                break;
-            default:
-                Log.e(TAG, "Do nothing");
-                break;
-        }
+    public void onCompletion(MediaPlayer mp) {
+        if (mp.isPlaying()) mp.stop();
+        donePlaying();
+        recordedFilenameView.setText("Completed!!!");
     }
 
     private void requestPermissions() {
@@ -117,39 +138,59 @@ public class AudioRecorderActivity extends AppCompatActivity implements View.OnC
     private void onClickRecord() {
         try {
             if (isRecording) {
-                action = "stop recording";
+                action = "prepare audio for playing";
                 stopRecording();
                 recordButton.setText("New Record");
                 isRecording = false;
-                recordedFilenameView.setText(audioFilename);
-                setMediaControllerVisible(true);
+                prepareAudioForPlaying();
             } else {
                 action = "start recording";
-                startRecording();
-                recordButton.setText("Stop Recording");
-                isRecording = true;
-                setMediaControllerVisible(false);
+                startRecordingIfPermissionGranted();
             }
         } catch (IOException e) {
-            Log.e(TAG, String.format("Failed to %s", action));
+            recordedFilenameView.setText(String.format("Failed to %s", action));
         }
     }
 
-    private void startRecording() throws IOException {
-        if (permissionToRecordGranted && permissionToStoreGranted) {
-            audioFilename = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
-            audioFilename += generateRandomFilename() + ".3gp";
-
-            mediaRecorder = new MediaRecorder();
-            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            mediaRecorder.setOutputFile(audioFilename);
-            mediaRecorder.prepare();
-            mediaRecorder.start();
-        } else {
-            requestPermissions();
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, activityPermissions[STORE_PERMISSION]) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(this, activityPermissions[RECORD_PERMISSION]) == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                requestPermissions();
+                return false;
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            return true;
         }
+    }
+
+    private void startRecordingIfPermissionGranted() throws IOException {
+        if (!hasPermission()) {
+            requestPermissions();
+            return;
+        }
+
+        startRecording();
+    }
+
+    private void startRecording() throws IOException {
+        audioFilename = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
+        audioFilename += "t4-" + generateRandomFilename() + ".3gp";
+
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setOutputFile(audioFilename);
+        mediaRecorder.prepare();
+        mediaRecorder.start();
+
+        recordButton.setText("Stop Recording");
+        recordedFilenameView.setText(audioFilename);
+        isRecording = true;
+        playButton.setEnabled(false);
     }
 
     private void stopRecording() {
@@ -158,100 +199,43 @@ public class AudioRecorderActivity extends AppCompatActivity implements View.OnC
         mediaRecorder = null;
     }
 
-    private void prepareRecordedAudio() throws IOException {
-        mediaPlayer = new MediaPlayer();
+    private void onClickPlay() {
+        if (isPlaying) {
+            stopPlaying();
+            donePlaying();
+            action = "stop playing audio";
+        } else {
+            mediaPlayer.seekTo(0);
+            mediaPlayer.start();
+            recordButton.setEnabled(false);
+            isPlaying = true;
+            playButton.setText("Stop Playing");
+            action = "play audio";
+        }
+    }
+
+    private void prepareAudioForPlaying() throws IOException {
+        mediaPlayer = MediaPlayer.create(this, Uri.parse(audioFilename));
         mediaPlayer.setScreenOnWhilePlaying(true);
         mediaPlayer.setOnPreparedListener(this);
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        mediaPlayer.setDataSource(audioFilename);
-        mediaPlayer.prepare();
+        mediaPlayer.setOnCompletionListener(this);
     }
 
     private void stopPlaying() {
-        mediaPlayer.stop();
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
     }
 
-    private void setMediaControllerVisible(boolean visible) {
-        mediaController.setEnabled(visible);
-
-        if (visible) {
-            mediaController.show();
-        } else {
-            mediaController.hide();
-        }
+    private void donePlaying() {
+        recordButton.setEnabled(true);
+        isPlaying = false;
+        playButton.setText("Play");
     }
 
     private static String generateRandomFilename() {
         Random random = new Random();
-        long randomValue = random.nextLong();
-        return "t4" + randomValue;
+        return String.valueOf(random.nextLong());
     }
 
-    @Override
-    public void start() {
-        try {
-            if (mediaPlayer != null) {
-                prepareRecordedAudio();
-                recordButton.setEnabled(false);
-            }
-        } catch(IOException e) {
-            Log.e(TAG, "Failed to prepare audio for playing");
-        }
-    }
-
-    @Override
-    public void pause() {
-        if (mediaPlayer != null) {
-            stopPlaying();
-        }
-        recordButton.setEnabled(true);
-    }
-
-    @Override
-    public int getDuration() {
-        return mediaPlayer == null ? 0 : mediaPlayer.getDuration();
-    }
-
-    @Override
-    public int getCurrentPosition() {
-        return mediaPlayer == null ? 0 : mediaPlayer.getCurrentPosition();
-    }
-
-    @Override
-    public void seekTo(int pos) {
-        if (mediaPlayer != null) {
-            mediaPlayer.seekTo(pos);
-        }
-    }
-
-    @Override
-    public boolean isPlaying() {
-        return mediaPlayer == null ? false : mediaPlayer.isPlaying();
-    }
-
-    @Override
-    public int getBufferPercentage() {
-        return 0;
-        //return mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration();
-    }
-
-    @Override
-    public boolean canPause() {
-        return false;
-    }
-
-    @Override
-    public boolean canSeekBackward() {
-        return false;
-    }
-
-    @Override
-    public boolean canSeekForward() {
-        return false;
-    }
-
-    @Override
-    public int getAudioSessionId() {
-        return mediaPlayer == null ? 0 : mediaPlayer.getAudioSessionId();
-    }
 }
