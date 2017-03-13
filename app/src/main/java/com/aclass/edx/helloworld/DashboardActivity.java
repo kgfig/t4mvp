9e8ee9eae30ff6efaa4be0dcf5296be20501102d
TicToc Dev <kathleengay.figueroa@gmail.com>
2017-03-13 19:34:12 +0800

    Add menu items in dashboard toolbar

--- app/src/main/java/com/aclass/edx/helloworld/DashboardActivity.java
+++ app/src/main/java/com/aclass/edx/helloworld/DashboardActivity.java
@@ -2,6 +2,10 @@ package com.aclass.edx.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.dashboard_toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dashboard, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemId = item.getItemId();
+
+        switch(menuItemId) {
+            case R.id.dashboard_action_profile:
+                Toast.makeText(this, "Go to profile", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.dashboard_action_practice:
+                Toast.makeText(this, "Practice", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
